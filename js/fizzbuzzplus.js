856003604a6870172d097b68541e3afe2d9cae13
Rodrigue <uluar@yahoo.fr>
2022-01-21 16:16:20 +0100

    Documentation de fizzbuzzplus.js

--- js/fizzbuzzplus.js
+++ js/fizzbuzzplus.js
@@ -1,3 +1,7 @@
+/**
+ * Une fonction qui attend un nombre en paramètre et retourne FIZZ si le nombre est mutiple de 3, BUZZ s'il est multiple de 5 ou FIZZBUZZ s'il est multiple de 3 et 5.
+ * @param { integer } number
+ */
 function fizzBuzz(number){
     let reponse = "";
 
@@ -17,10 +21,19 @@ function fizzBuzz(number){
     return reponse;
 }
 
+// Initialisation variable qui gère le nombre de clics
 let nombreClics = 0;
 
+
+/**
+ * Une fonction qui permet d'incrémenter la valeur afficher dans le DOM
+ * 
+ */
 function incrementOneNumber() {
+    // Récuperation de l'element DIV par son ID
     let compteur = document.getElementById("resultat");
+    
+    // Test de la valeur afficher dans le DOM et affectation de la valeur
     if (compteur.textContent == ""){
         nombreClics;
         compteur.innerHTML = nombreClics +" : "+fizzBuzz(nombreClics);
@@ -31,12 +44,21 @@ function incrementOneNumber() {
     }
 }
 
-
+/**
+ * Une fonction qui permet de décrémenter la valeur afficher dans le DOM
+ * 
+ */
 function decrementOneNumber() {
+    // Récuperation de l'element DIV par son ID
     let compteur = document.getElementById("resultat");
+    
+    // Décrémentation
     nombreClics--;
+
+    // Affectation de la valeur dans le DOM
     compteur.innerHTML = nombreClics +" : "+fizzBuzz(nombreClics);
 }
 
+// Appel des fonctions
 decrementOneNumber;
 incrementOneNumber;
