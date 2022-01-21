13aa306d3acde3273d8a63f249621f95e16eafe1
Rodrigue <uluar@yahoo.fr>
2022-01-21 16:03:18 +0100

    Documentation fizzbuzz.js

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,3 +1,8 @@
+/**
+ * Une fonction qui attend un nombre en paramètre et retourne FIZZ si le nombre est mutiple de 3, BUZZ s'il est multiple de 5 ou FIZZBUZZ s'il est multiple de 3 et 5.
+ * @param { integer } number
+ */
+
 function fizzBuzz(number){
     let reponse = "";
 
@@ -17,6 +22,10 @@ function fizzBuzz(number){
     return reponse;
 }
 
+
+/**
+ * Une fonction qui crée un élement P dans le DOM en appellant la fonction fizzBuzz en lui passant de nombre en paramètre de 0 à 100
+ */
 function incrementNumber(){
     let div = document.getElementById("resultats");
     for(i = 0; i <= 100; i++){
@@ -28,4 +37,5 @@ function incrementNumber(){
     }
 }
 
+// appel de la fonction
 incrementNumber();
