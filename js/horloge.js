eea15a121060234276a7307bbd525be243f2efe0
Rodrigue <uluar@yahoo.fr>
2022-01-21 15:41:32 +0100

    Documentation de horloge.js

--- js/horloge.js
+++ js/horloge.js
@@ -1,9 +1,17 @@
-function getDateTime() {
-    var now     = new Date(); 
-    var hour    = now.getHours();
-    var minute  = now.getMinutes();
-    var second  = now.getSeconds(); 
+/** 
+*Une fonction qui retourne en type string l'heure actuelle 
+*
+*/
+function getTime() {
+     // Instanciation de la classe Date
+    let now     = new Date(); 
+
+    // Récuperation des methodes getHours, getMinutes et getSeconds de la classe Date
+    let hour    = now.getHours();
+    let minute  = now.getMinutes();
+    let second  = now.getSeconds(); 
   
+    // Gestion de l'affichage de l'heure, minute et seconde inférieure à 10
     if(hour.toString().length == 1) {
          hour = '0'+hour;
     }
@@ -15,11 +23,17 @@ function getDateTime() {
     if(second.toString().length == 1) {
          second = '0'+second;
     }   
-    var dateTime = hour+':'+minute+':'+second;   
-     return dateTime;
+
+    // Concaténation de l'heure, minute et seconde
+    let time = hour+':'+minute+':'+second;   
+    
+    // L'heure retournée
+    return time;
 }
 
+
+// L'appel de la fonction getTime à chaque seconde
 setInterval(function(){
-    currentTime = getDateTime();
+    currentTime = getTime();
     document.getElementById("heure").innerHTML = currentTime;
 }, 1000);
